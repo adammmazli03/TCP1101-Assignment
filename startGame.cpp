ad78e1d772eab86a3f38c5dd66e68e1971f4bea8
adam mazli <1211101073@student.mmu.edu.my>
2023-01-29 23:18:30 +0800

    Function for Game Main Menu

--- startGame.cpp
+++ startGame.cpp
@@ -1,3 +1,13 @@
+// *********************************************************
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS
+// Year: Trimester 1, 2022/23 (T2215)
+// Lab: T10L, Group 5
+// Names: MUHAMMAD ADAM BIN MAZLI ZAKUAN | AHMAD NUR IKHWAN BIN HAMID | MUHAMMAD AL-AMIN BIN MOHD NORMARZUKI
+// IDs: 1211101073 | 1211103140 | 1211103201
+// Emails: 1211101073@student.mmu.edu.my | 1211103140@student.mmu.edu.my | 1211103201@student.mmu.edu.my
+// Phones: 019-729-5420 | 011-5890-0328 | 011-2704-8621
+// *********************************************************
+
 #include <iostream>
 #include <string>
 #include <cstdlib>
